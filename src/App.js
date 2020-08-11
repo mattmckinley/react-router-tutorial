7ea3ca9469bc9dda5e819e8a86c1164727a0b831
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-08-11 13:06:47 +0100

    learning react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Link, Route, Switch } from "react-router-dom";
 import './App.css';
+import Category from './Category';
+
+// const Home = () => (
+//   <div>
+//     <h2>Home</h2>
+//   </div>
+// );
+
+// const Category = () => (
+//   <div>
+//     <h2>Category</h2>
+//   </div>
+// );
+
+// const Products = () => (
+//   <div>
+//     <h2>Products</h2>
+//   </div>
+// );
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/category">Category</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+      {/* <Route exact={true} path="/" component={Home} /> */}
+      <Route path="/category" component={Category} />
+      {/* <Route path="/products" component={Products} /> */}
+      </Switch>
     </div>
   );
 }
